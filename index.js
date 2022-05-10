3cbd657a60e79db59bfbe210daf397f9a5997a3b
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-05-10 16:37:11 +0200

    First minimal version

---
+++ index.js
@@ -0,0 +1,32 @@
+let numberOfShapes = 5
+let shapesArray = []
+let container = document.getElementById("container")
+
+for (let i=0; i < numberOfShapes; i++) {
+    shapesArray.push(i)
+}
+
+for (let i=0; i < shapesArray.length; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute('id', `shape${i+1}`);
+    const newContent = document.createTextNode(`Shape${i+1}`);
+    newDiv.appendChild(newContent);
+    container.appendChild(newDiv);
+    newDiv.classList.add("shape");
+    let topRight = Math.floor((Math.random())*16)+5
+    let topLeft = Math.floor(Math.random()*16)+5
+    let bottomRight = Math.floor(Math.random()*16)+5
+    let bottomLeft = Math.floor(Math.random()*16)+5
+    let width = Math.floor(Math.random()*80)+40
+    console.log(width)
+    newDiv.style.cssText += 
+    `width:${width}px;border-radius:${topLeft}em ${topRight}em ${bottomLeft}em ${bottomRight}em;`;
+}
+
+
+
+
+
+
+
+
